3cae65367b29819e5d5ec25625dffcd9f0a195e2
Sergey Efimov <s.efimov@skoltech.ru>
2019-04-13 18:34:56 +0300

    Create some structure

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
-import Menu from "./Components/Menu";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Menu />
-        
-        <Route exact path="/" component={CommonComponent} />
-        <Route path="/about" component={CommonComponent} />
-        <Route path="/topics" component={CommonComponent} />
-        <Route path="/tolstovki/" component={CommonComponent} />
-        <Route path="/svitera/" component={CommonComponent} />
-        <Route path="/delivery/" component={CommonComponent} />
-        <Route path="/contacts/" component={CommonComponent} />
+      <div className="container">
+        <Header />
+        <Main />
+        <Footer />
       </div>
     </Router>
   );
-}
-
-// TODO: Temporary component. Should be deleted.
-function CommonComponent ({ match }) {
-  function createHeader () {
-    return match.url.slice(1);
-  }
-  
-  return(
-    <h1 className={'header'}>{createHeader()}</h1>
-  )
 }
 
 export default App;
--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,30 +1,17 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Menu({ match }) {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-      <li>
-        <Link to="/tolstovki">Tolstovki</Link>
-      </li>
-      <li>
-        <Link to="/svitera">Svitera</Link>
-      </li>
-      <li>
-        <Link to="/delivery">Delivery</Link>
-      </li>
-      <li>
-        <Link to="/contacts">Contacts</Link>
-      </li>
-    </ul>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/topics">Topics</Link>
+      <Link to="/catalog">Masks</Link>
+      <Link to="/tolstovki">Tolstovki</Link>
+      <Link to="/svitera">Svitera</Link>
+      <Link to="/delivery">Delivery</Link>
+      <Link to="/contacts">Contacts</Link>
+    </nav>
   );
 }
